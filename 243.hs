e9252e4f4101a565c8d3651e1fca1eabc0b8b2d5
Daniel Schmitz <daniel2.schmitz@tu-dortmund.de>
2014-11-20 17:46:26 +0100

    2048

--- 243.hs
+++ 243.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 import System.IO
 import Data.Char (ord)
 import Control.Monad
@@ -7,8 +8,24 @@ import Data.List (intersperse,intercalate,transpose)
 import System.Random (getStdRandom,randomR)
 import System.Console.ANSI
  
+#ifdef hs2048
+dim = 5
+base = 2
+winat = 11
+p :: Float -> Int
+p x |   x < (1/7)*4 = 0
+    |   x < (1/7)*6 = 1
+    | otherwise = 2
+#else
 dim = 5
 base = 3
+winat = 5
+p :: Float -> Int
+p x |   x < 0.9 = 0
+    | otherwise = 1
+#endif
+
+winAt = base^winat
 r = [0..dim-1]
 
 type Brett = [[Int]]
@@ -27,9 +44,6 @@ neuesElement b = besetze <$> zufall1oder3 <*> wähle [(y,x) | x <- r, y <- r, b 
           wähle l =  (!!) l <$> randr (length l - 1)
           zufall1oder3 :: IO Int
           zufall1oder3 = (base^) . p <$> randr 1
-              where p :: Float -> Int
-                    p x |   x < 0.9 = 0
-                        | otherwise = 1
 
 schritt :: Ar -> Brett -> IO Brett
 schritt ar b = let b' = schiebe ar b
@@ -38,12 +52,12 @@ schritt ar b = let b' = schiebe ar b
 
 sl :: [Int] -> [Int]
 sl = padd . merge . filter (/= 0) 
-    where merge (x1:x2:x3:xs) | x1 == x2 && x1 == x3 = x1+x2+x3 : xs
-                              | otherwise = x1 : merge (x2:x3:xs)
-          merge (x:xs) = x : merge xs
+    where merge (x:xs) | length xs >=  bm1 && all (==x) (take bm1 xs) = x*base : merge (drop bm1 xs)
+                       | otherwise = x : merge xs
           merge [] = []
           padd xs | length xs < dim = xs ++ replicate (dim - length xs) 0
                   | otherwise = xs
+          bm1 = base - 1
 
 schiebe :: Ar -> Brett -> Brett
 schiebe (Verti,r)     = transpose . schiebe (Hori,r) . transpose
@@ -56,22 +70,23 @@ isOver b = all ( (==b) . flip schiebe b ) alleRichtungen
           alleRichtungen = (,) <$> [minBound .. ] <*> [minBound .. ]
 
 isWon :: Brett -> Bool
-isWon = any (elem 243)
+isWon = (any . elem) winAt
 
 zeigeBrett :: Brett -> IO ()
 zeigeBrett b = clearScreen >> zb' b >> putStrLn "" >> putStrLn ""
     where zb' = sequence_ . intersperse linesep . map 
                 (sequence_ . intersperse colsep . map show')
-          linesep = putStrLn "" >> colorStr Black (replicate (dim*4-1) '-') >> putStrLn ""
+          linesep = putStrLn "" >> colorStr Black (replicate (dim*(alignat+1)-1) '-') >> putStrLn ""
           colsep = colorStr Black "|"
           show' :: Int -> IO () 
-          show' 0 = colorStr Black "   "
+          show' 0 = colorStr Black (replicate alignat ' ')
           show' x = colorStr (farbe x) $ (fillup . show) x
-            where fillup x = let tofill = 3 - length x 
+            where fillup x = let tofill = alignat - length x 
                               in if tofill > 0 then replicate tofill ' ' ++ x
                                                else x
+          alignat = (length.show) winAt
           farbe x = let found = [c | (n,c) <- zip [0..] 
-                                    [White,Cyan,Magenta,Blue,Yellow,Green],3^n == x]
+                                    [White,Cyan,Magenta,Blue,Yellow,Green],base^n == x]
                     in if null found then Black else head found
           colorStr fg str = do
               setSGR [SetColor Foreground Vivid fg, SetColor Background Dull Yellow]
@@ -94,7 +109,7 @@ main = do
     actualmain
 
 actualmain = neuesElement leeresBrett >>= neuesElement >>= zeigeBrett `s` loop 0 
-    where s f g = \x -> f x >> g x
+    where s f g x = f x >> g x
           loop z brett = getChar >>= flip reagiereaufEingabe brett . verarbeiteEingabe z . ord 
           reagiereaufEingabe (z,Nothing) =  loop z 
           reagiereaufEingabe (z,Just ar) =  schritt ar >=> zeigeBrett `s` behandleRandfälle z
